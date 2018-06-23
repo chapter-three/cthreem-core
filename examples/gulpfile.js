6505d45b8b8257fd8b3172b513300a20aab2da73
Rob Decker <rob@deckerdev.com>
2018-06-23 12:21:29 -0700

    Add available tasks to example gulpfile

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -4,3 +4,12 @@ const gulp = require('gulp');
 const userConfig = require('./gulp-config');
 
 require('cthreem-core')(gulp, userConfig);
+
+
+// Available gulp tasks:
+// - clean
+// - compile
+// - compress
+// - validate
+// - watch
+// - default: uses each of the above tasks
