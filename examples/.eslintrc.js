4f5328059471da1c9d810a5a29482393e2e9a70a
Rob Decker <rob@deckerdev.com>
2018-07-12 10:20:53 -0700

    Keep eslint from "seeing" parent eslintrc files

--- examples/.eslintrc.js
+++ examples/.eslintrc.js
@@ -1,6 +1,7 @@
 // See https://eslint.org/docs/user-guide/configuring#using-configuration-files
 
 module.exports = {
+  root: true,
   extends: "eslint:recommended",
   globals: {
     Drupal: true,
